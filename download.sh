c7af80f5e92a65f37467445fe01b5a8d2b8a533d
saichaithanya <saichaithanya314@gmail.com>
2022-06-10 15:39:17 +0530

    Added option for custom path

--- download.sh
+++ download.sh
@@ -5,6 +5,6 @@ pip install wheel &&
 pip install --no-cache-dir -r requirements.txt &&
 path="$(pwd)" &&
 path="${path}/magoosh_videos" &&
-python3 get_magoosh.py  ${path} &&
+python3 get_magoosh.py  ${1:-${path}} &&
 deactivate &&
 rm -rf magoosh
